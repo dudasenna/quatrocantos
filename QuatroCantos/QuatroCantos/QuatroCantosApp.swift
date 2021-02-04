1fc719697f4d3cddacd00141c1fb369138e2fca7
Mayara Souza <mms8@cin.ufpe.br>
2021-02-04 15:10:55 -0300

    Adiciona projeto

---
+++ QuatroCantos/QuatroCantos/QuatroCantosApp.swift
@@ -0,0 +1,20 @@
+//
+//  QuatroCantosApp.swift
+//  QuatroCantos
+//
+//  Created by Mayara Mendonça de Souza on 04/02/21.
+//
+
+import SwiftUI
+
+@main
+struct QuatroCantosApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}

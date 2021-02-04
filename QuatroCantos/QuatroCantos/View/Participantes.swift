ef54a720cb788bdcd468b66eafc96c8d0e204821
Mayara Souza <mms8@cin.ufpe.br>
2021-02-04 18:05:48 -0300

    Adiciona a tela de seleção de quantidade de participantes

--- QuatroCantos/QuatroCantos/View/Participantes.swift
+++ QuatroCantos/QuatroCantos/View/Participantes.swift
@@ -7,10 +7,47 @@
 
 import SwiftUI
 
+struct Box {
+    var id: Int
+    var imageUrl: String
+}
+
 struct Participantes: View {
+    let boxes: [Box] = [
+        Box(id: 0, imageUrl: "1"),
+        Box(id: 1, imageUrl: "2"),
+        Box(id: 2, imageUrl: "3"),
+        Box(id: 3, imageUrl: "4"),
+        Box(id: 4, imageUrl: "5"),
+        Box(id: 5, imageUrl: "6")
+    ]
+    
     var body: some View {
-        Text("Selecione a quantidade de pessoas")
-            .padding()
+        Text("Quantas pessoas vão brincar?")
+            .font(.title2)
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+        ScrollView(.horizontal) {
+            HStack {
+                ForEach(boxes, id: \.id) {
+                    box in BoxView(box: box).onTapGesture {
+                        let numeroParticipantes = box.id + 1
+                        print("O número de pessoas que vai brincar é \(numeroParticipantes)")
+                    }
+                }.padding(30)
+            }
+        }
+        Text("Próximo")
+    }
+}
+
+struct BoxView: View {
+    let box: Box
+    
+    var body: some View {
+        Image("\(box.imageUrl)")
+            .resizable()
+            .cornerRadius(12)
+            .frame(width: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100/*@END_MENU_TOKEN@*/)
     }
 }
 

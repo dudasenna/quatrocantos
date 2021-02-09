643d6ff0813af2b039fa6c3a23447c5fbb65f8ca
Mayara Souza <mms8@cin.ufpe.br>
2021-02-09 17:08:32 -0300

    Adiciona a navegação entre telas

---
+++ QuatroCantos/QuatroCantos/View/ViewRouter.swift
@@ -0,0 +1,13 @@
+//
+//  ViewRouter.swift
+//  QuatroCantos
+//
+//  Created by Mayara Mendonça de Souza on 09/02/21.
+//
+
+import SwiftUI
+
+class ViewRouter: ObservableObject {
+    
+    @Published var currentPage:Page = .page1
+}

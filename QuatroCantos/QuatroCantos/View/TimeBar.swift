92dc7650c96a27e516361e0606c423258f58b354
Palloma Ramos <pallomaramos@MacBook-Pro-de-Palloma.local>
2021-02-12 13:42:35 -0300

    UI da tela principal

--- QuatroCantos/QuatroCantos/View/TimeBar.swift
+++ QuatroCantos/QuatroCantos/View/TimeBar.swift
@@ -13,17 +13,21 @@ struct ProgressBar: View {
     
     @State private var isClicked: Bool = false
     
+    
+    let greenTimer = Color(red: 10/255, green: 160/255, blue: 136/255, opacity: 1.0)
+    
     var body: some View {
         GeometryReader { geometry in
             ZStack(alignment: .leading) {
                 Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
-                    .opacity(0.1)
+                    .opacity(0)
+                    .border(Color.white, width: 2)
                 Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
-                    .foregroundColor(Color(UIColor.white))
+                    .foregroundColor(greenTimer)
                     .animation(.default)
-                Image("confete 1").padding(.leading,380)
-            }.cornerRadius(60)
-            .padding(.top, -170)
+                //                Image("confete 1").padding(.leading,380)
+            }.cornerRadius(2)
+            .padding(.top, -130)
         }.frame(height:9)
         .frame(width:400)
         Spacer()
@@ -46,11 +50,15 @@ struct TimeBar: View {
     @State var counter4 = 1
     @State var counter5 = 1
     
-    let bgcolor = Color(red: 68/255, green: 181/255, blue: 163/255, opacity: 1.0)
-    
+    //    let bgcolor = Color(red: 68/255, green: 181/255, blue: 163/255, opacity: 1.0)
+    //
     let perrengues: [String] = ["Já perdi o meu chinelo em Olinda", "Quase morri sufocade com o aperto", "Troquei Olinda pelo Galo da Madrugada", "Já voltei sentado no chão do ônibus"]
     @State var perrengueAleatorio:String = "Já fui no bloco da Lama"
     @State var contadorAuxiliar:Int = 0
+    
+    let bg_timer: [Image] = [Image("bg_timer2"),Image("bg_timer3"),Image("bg_timer4"),Image("bg_timer5"),Image("bg_timer6"),Image("bg_timer7")]
+    @State var bgAleatório: Image = Image("bg_timer1")
+    @State var bgContador: Int = 0
     
     let hapitcs = UINotificationFeedbackGenerator()
     
@@ -58,9 +66,12 @@ struct TimeBar: View {
     
     var body: some View {
         ZStack {
-            bgcolor
-            ProgressBar(value: progressBarValue)
+            bgAleatório
+                .resizable()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
             
+            ProgressBar(value: progressBarValue)
             
             Color.white
                 .frame(width: 583, height: 245, alignment: .bottom)
@@ -72,6 +83,7 @@ struct TimeBar: View {
                 .animation(.spring())
             
             Text(perrengueAleatorio).fontWeight(.semibold).font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/).foregroundColor(.black)
+            
             
             Button(action: {
                 respondeu()
@@ -124,6 +136,12 @@ struct TimeBar: View {
             self.perrengueAleatorio = self.perrengues[contadorAuxiliar]
             print("valor do contador depois de atualizar é \(contadorAuxiliar)" )
         }
+        if (bgContador < bg_timer.count){
+            print("valor do contador é \(bgContador)" )
+            self.bgAleatório = self.bg_timer[bgContador]
+            print("valor do contador depois de atualizar é \(bgContador)" )
+        }
+        self.bgContador += 1
         self.contadorAuxiliar += 1
         if (contadorAuxiliar == perrengues.count+1){
             viewRouter.currentPage = .page4
